dd49c859f745a828864c9a20131caf20c7b9c029
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-06-24 16:36:52 +0330

    Add project files.

---
+++ MiddlewareSample/CustomMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddlewareSample
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        public CustomMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<CustomMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            _logger.LogInformation("Log from CustomMiddleware Before");
+            await _next(httpContext);
+            _logger.LogInformation("Log from CustomMiddleware After");
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
---
+++ MiddlewareSample/CustomMiddleware2.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MiddlewareSample
+{
+    public class CustomMiddleware2 : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("CustomMiddleware2 before");
+            await next(context);
+            await context.Response.WriteAsync("CustomMiddleware2 after");
+        }
+    }
+
+    public static class Extention
+    {
+        public static IApplicationBuilder UseCustomMiddleware2(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware2>();
+        }
+    }
+}
